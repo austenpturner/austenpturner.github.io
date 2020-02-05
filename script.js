74ffa3549272f330c6ca2ceb8a3c3c65f840dc95
Austen Turner <austenpturner@msn.com>
2020-02-05 11:00:08 -0800

    add chevron arrow and animation

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const navSymbol = $('#nav-symbol');
 navSymbol.click(function() {
     if ($(this).prev().hasClass('slide-right')) {
         $(this).prev().removeClass('slide-right').addClass('slide-left');
-        console.log($(this).prev());
     } else if ($(this).prev().hasClass('slide-left')) {
         $(this).prev().removeClass('slide-left').addClass('slide-right');
-        console.log($(this).prev());
     }
 })
