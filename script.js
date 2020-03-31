6152b48c1b4ba61dcfb003a9e5db18d09cbe42e8
Austen Turner <austenpturner@msn.com>
2020-03-30 19:18:44 -0700

    fix portfolio section responsive layout

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 // DOM Elements
 const navSymbol = document.querySelector('#nav-symbol');
+const navLinks = document.querySelector('#nav-links');
 
-// Slide nav on and off screen
+// Slide nav on and off screen when user clicks nav symbol
 navSymbol.addEventListener('click', () => {
-    console.log(navSymbol.previousElementSibling.className);
     if (navSymbol.previousElementSibling.className === 'slide-right') {
         navSymbol.previousElementSibling.classList.remove('slide-right');
         navSymbol.previousElementSibling.classList.add('slide-left');
     } else if (navSymbol.previousElementSibling.className === 'slide-left') {
+        navSymbol.previousElementSibling.classList.remove('slide-left');
+        navSymbol.previousElementSibling.classList.add('slide-right');
+    }
+});
+
+// Slide nav off screen when user clicks nav item
+navLinks.addEventListener('click', e => {
+    if (e.target.className === 'nav-link') {
         navSymbol.previousElementSibling.classList.remove('slide-left');
         navSymbol.previousElementSibling.classList.add('slide-right');
     }
