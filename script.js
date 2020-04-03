5c4578b091c2611d33396eeed9935fd06a1a5038
Austen Turner <austenpturner@msn.com>
2020-04-03 13:56:04 -0700

    add fixed and responsive nav bar

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ const navLinks = document.querySelector('#nav-links');
 
 // Slide nav on and off screen when user clicks nav symbol
 navSymbol.addEventListener('click', () => {
-    if (navSymbol.previousElementSibling.className === 'slide-right') {
-        navSymbol.previousElementSibling.classList.remove('slide-right');
-        navSymbol.previousElementSibling.classList.add('slide-left');
-    } else if (navSymbol.previousElementSibling.className === 'slide-left') {
-        navSymbol.previousElementSibling.classList.remove('slide-left');
-        navSymbol.previousElementSibling.classList.add('slide-right');
+    console.log('clicked nav symbol');
+        if (navLinks.className === 'slide-right') {
+        navLinks.classList.remove('slide-right');
+        navLinks.classList.add('slide-left');
+    } else if (navLinks.className === 'slide-left') {
+        navLinks.classList.remove('slide-left');
+        navLinks.classList.add('slide-right');
     }
 });
 
 // Slide nav off screen when user clicks nav item
 navLinks.addEventListener('click', e => {
     if (e.target.className === 'nav-link') {
-        navSymbol.previousElementSibling.classList.remove('slide-left');
-        navSymbol.previousElementSibling.classList.add('slide-right');
+        navLinks.classList.remove('slide-left');
+        navLinks.classList.add('slide-right');
     }
 });
 
